c093bc320030cb7d1fcc7a7ad02b949adb479a10
gabrielli-lima <gabriellilima165@gmail.com>
2023-11-23 16:12:36 -0300

    ✨ feat: add menu mobile

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const modal = document.getElementById("modal");
-const modalConteudo = document.getElementById("modalConteudo");
+var headerConteudo = document.getElementById('headerConteudo');
+var menu = document.getElementById('menu');
+var navBar = document.getElementById('navBar');
+var iconMenu = document.getElementById('iconMenu');
+var menuImage = document.getElementById('menuImage');
 
-function modalC() {
-  modal.style.visibility = "visible"
-  modal.style.opacity = "1"
-  modalConteudo.style.visibility = "visible"
-  modalConteudo.style.opacity = "1"
-}
+iconMenu.addEventListener('click', function () {
+  if (menuImage.src.includes('list.svg')) {
+    menuImage.src = './assets/close.svg';
 
-function btnClose(){
-  modal.style.visibility = "hidden"
-  modal.style.opacity = "0"
-  modalConteudo.style.visibility = "hidden"
-  modalConteudo.style.opacity = "0"
-}
+    navBar.classList.add('fixed', 'shadow-lg');
+    headerConteudo.classList.add('pt-[88px]')
+
+    menu.classList.remove('hidden');
+    menu.classList.add('fixed', 'flex', 'flex-row', 'justify-end');
+
+  } else {
+    menuImage.src = './assets/list.svg';
+    
+    navBar.classList.remove('fixed', 'shadow-lg');
+    headerConteudo.classList.remove('pt-[88px]')
+
+    menu.classList.add('hidden');
+    menu.classList.remove('fixed', 'flex', 'flex-row', 'flex-end');
+  }
+
+});

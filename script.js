71f6c6c2ff0e83598178895790244381d534b828
gabrielli-lima <gabriellilima165@gmail.com>
2023-11-24 17:28:26 -0300

    ✨ feat: add secao 'ver todos'

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ var navBar = document.getElementById('navBar');
 var iconMenu = document.getElementById('iconMenu');
 var menuImage = document.getElementById('menuImage');
 
+var btVerTodos = document.getElementById('btVerTodos');
+var maisProjetos = document.getElementById('maisProjetos')
+var textoBtVerTodos = document.getElementById('textoBtVerTodos')
+
 iconMenu.addEventListener('click', function () {
   if (menuImage.src.includes('list.svg')) {
     menuImage.src = './assets/close.svg';
@@ -16,7 +20,7 @@ iconMenu.addEventListener('click', function () {
 
   } else {
     menuImage.src = './assets/list.svg';
-    
+
     navBar.classList.remove('fixed', 'shadow-lg');
     headerConteudo.classList.remove('pt-[88px]')
 
@@ -25,3 +29,16 @@ iconMenu.addEventListener('click', function () {
   }
 
 });
+
+maisProjetos.style.display = 'none';
+btVerTodos.onclick = function () {
+  if (maisProjetos.style.display === 'none') {
+    maisProjetos.style.display = 'flex';
+
+    textoBtVerTodos.innerText = 'Mostrar menos'
+    menuImage.src = './assets/list.svg';
+  } else {
+    maisProjetos.style.display = 'none';
+    textoBtVerTodos.innerText = 'Ver todos'
+  }
+}

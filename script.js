ccec884c499eca4e8496fa3d0bb0a4cc4a65f84d
gabrielli-lima <gabriellilima165@gmail.com>
2023-11-26 17:04:12 -0300

    🌈 style: aplicando responsividade no footer e scroll behavior

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var menuImage = document.getElementById('menuImage');
 var btVerTodos = document.getElementById('btVerTodos');
 var maisProjetos = document.getElementById('maisProjetos')
 var textoBtVerTodos = document.getElementById('textoBtVerTodos')
+var projetos = document.getElementById('projetos');
 
 var cardProjeto = document.querySelectorAll('.card-projeto')
 
@@ -40,6 +41,7 @@ btVerTodos.onclick = function () {
     textoBtVerTodos.innerText = 'Mostrar menos'
     menuImage.src = './assets/list.svg';
   } else {
+    projetos.scrollIntoView();
     maisProjetos.style.display = 'none';
     textoBtVerTodos.innerText = 'Ver todos'
   }
